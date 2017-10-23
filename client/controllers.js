44614442f02a5e94b7a669ddf175df733e8cdfc0
smebdi <xcmdprompt@gmail.com>
2017-10-23 13:15:31 -0500

    heroku changes

--- client/controllers.js
+++ client/controllers.js
@@ -1,4 +1,4 @@
-angular.module('store.controllers', [])
+angular.module('store.controllers', ['ngRoute', 'ngResource'])
 .controller('WelcomeController', ['$scope', function($scope) {
     $scope.message = "Hello from Angular";
 }]);
