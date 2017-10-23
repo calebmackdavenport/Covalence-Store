1f38cbb2496f84353d21ea781930ec8c47f3b58a
smebdi <xcmdprompt@gmail.com>
2017-10-23 13:20:10 -0500

    index update

--- server/index.ts
+++ server/index.ts
@@ -7,7 +7,6 @@ const app = express();
 
 app.use(express.static(clientPath));
 
-console.log(process.env.PORT || 3000);
-app.listen(3000, () => {
-    console.log("listening on 3k");
+app.listen(process.env.PORT || 3000, () => {
+    console.log("listening");
 });
