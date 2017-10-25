bb9e0376eada8a3f7d7e179685531f08dd0ff793
smebdi <xcmdprompt@gmail.com>
2017-10-25 15:17:15 -0500

    emergency changes

--- server/index.ts
+++ server/index.ts
@@ -2,8 +2,8 @@ import {join} from 'path';
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as cookieParser from 'cookie-parser';
-var routing = require('./middleware/routing.mw');
-var api = require('./api');
+import stateRouting from './middleware/routing.mw';
+import apiRouter from './api';
 var prerender = require('prerender-node');
 
 
@@ -15,8 +15,8 @@ app.use(prerender);
 app.use(express.static(clientPath));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use('/api', api);
-app.get('*', routing.stateRouting);
+app.use('/api', apiRouter);
+app.get('*', stateRouting);
 
 
 app.listen(process.env.PORT || 3000, () => {
